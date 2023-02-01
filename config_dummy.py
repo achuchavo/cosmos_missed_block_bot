affc44fcde3093e16b72a784f89511d2224186d7
Achu Muamaah <achumuamah@gmail.com>
2023-02-01 18:01:13 +0900

    Update config_dummy.py

--- config_dummy.py
+++ config_dummy.py
@@ -5,5 +5,5 @@ rpc_osmo = 'https://osmosis-rpc.polkachu.com'
 rpc_evmos = 'https://tendermint.bd.evmos.org:26657'
 val_evmos = 'C52CF80E872A4F2CC3A114A733D301A421C857F0'
 val_osmo = '3749086B6D85BDE3DACFBE4485E3DF95E709B6DB'
-uptime_limit =  96
-uptime_limit_fixed =  96
+uptime_limit =  90
+uptime_limit_fixed =  90
