fecabba7e73082d087546f06d6085f75edb2f4bc
achuchavo <achumuamah@gmail.com>
2023-02-01 17:58:54 +0900

    add config dummy

---
+++ config_dummy.py
@@ -0,0 +1,9 @@
+
+TOKEN = "telegram bot token"
+the_chat_id = "-881802204"
+rpc_osmo = 'https://osmosis-rpc.polkachu.com'
+rpc_evmos = 'https://tendermint.bd.evmos.org:26657'
+val_evmos = 'C52CF80E872A4F2CC3A114A733D301A421C857F0'
+val_osmo = '3749086B6D85BDE3DACFBE4485E3DF95E709B6DB'
+uptime_limit =  96
+uptime_limit_fixed =  96
